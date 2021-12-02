0fe346dd8a989bb7199a1653c092d26c412a28a0
a2uuz <aziznageeye9@gmail.com>
2021-12-02 23:40:24 +0300

    guesing game in somali

---
+++ script.js
@@ -0,0 +1,40 @@
+let kuNasiibi = false;
+var lambar = 3;
+var nasiibi = prompt(`ku nasiibi lambar 1 - 10 !`);
+
+if (+nasiibi === lambar){
+  kuNasiibi = true;
+}
+
+if(kuNasiibi === true){
+alert(`libax yaa kuu shegay inay jawabtu tahay ${lambar}`);
+}else{
+alert(`jeejee`);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//if () {
+//  console.log('The condition is true.');
+//} else {
+//  console.log('The condition is false.');
+//}
